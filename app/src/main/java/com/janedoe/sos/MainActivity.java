b8f849a813213bd8d35786b344ccba1e47a6b6b7
Valentine <valentineb@bluewin.ch>
2016-08-04 16:18:15 +0100

    Help button and open camera

--- app/src/main/java/com/janedoe/sos/MainActivity.java
+++ app/src/main/java/com/janedoe/sos/MainActivity.java
@@ -2,12 +2,39 @@ package com.janedoe.sos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
+import android.provider.*;
+import android.net.Uri;
+import java.io.File;
+import android.os.Environment;
+import android.app.Activity;
+import android.graphics.*;
+import android.util.Log;
+import android.content.pm.*;
+import android.content.pm.PackageManager.*;
+import android.support.v4.content.*;
+import android.support.v4.app.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button help_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        help_button = (Button) findViewById(R.id.help_button);
+
+        help_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent camera = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(camera, 0);
+            }
+
+        });
     }
 }
