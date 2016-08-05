2124258fc1b58c5182155eecb7fed667f35f3755
olamedrek <olamed@op.pl>
2016-08-04 22:19:03 -0700

    Corrections

--- app/src/main/java/com/janedoe/sos/EventDetailsActivity.java
+++ app/src/main/java/com/janedoe/sos/EventDetailsActivity.java
@@ -34,137 +34,147 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+
+
+/** Assumptions:
+ * everything in database can be null except the location. user cannot put event with null location
+ * into database. the popup with message is shown.
+ */
 
 public class EventDetailsActivity extends AppCompatActivity {
 
-        private Button sendButton;
-        private Button photoButton;
-        private EditText editText;
+    private Button sendButton;
+    private Button photoButton;
+    private EditText editText;
 
-        private DatabaseReference database = FirebaseDatabase.getInstance().getReference();
-        private StorageReference storage = FirebaseStorage.getInstance().getReference();
+    private DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    private StorageReference storage = FirebaseStorage.getInstance().getReference();
 
-        private Uri photoUri = Uri.parse("");
+    private Uri photoUri = Uri.parse("");
+    private byte[] photo = new byte[2];
 
-        private byte[] photo = new byte[2];
-        private String time;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_event_details);
+        sendButton = (Button) findViewById(R.id.sendbutton);
+        photoButton = (Button) findViewById(R.id.photobutton);
+        editText = (EditText) findViewById(R.id.edittext);
 
-            sendButton = (Button) findViewById(R.id.sendbutton);
-            photoButton = (Button) findViewById(R.id.photobutton);
-            editText = (EditText) findViewById(R.id.edittext);
-
-            sendButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String message = editText.getText().toString();
-                    addEventToDatabase(getCurrentLocation(), message, time, photo);
-
-                    AlertDialog.Builder dialog  = new AlertDialog.Builder(EventDetailsActivity.this);
-                    dialog.setMessage("Message sent");
-                    dialog.setPositiveButton("OK", null);
-                    dialog.create().show();
-
-                    dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                }
-            });
-
-            photoButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    takePicture();
-                }
-            });
-        }
-
-
-        private String addEventToDatabase(String location, String time, String message, byte[] photo) {
-            DatabaseReference events = database.child("events");
-            DatabaseReference newEvent = events.push();
-
-            uploadPicture(photo, newEvent.getKey());
-            Event event = new Event(location, time, message, photoUri.toString());
-
-            newEvent.setValue(event);
-            return newEvent.getKey();
-        }
-
-        /**
-         * uploads picture and sets its uri in global variable photoUri;
-         * photoUri is not changed when uploading does not succeed
-         * @param photo can be null
-         * @param key unique key for object we want to add to database, used to create unique path for photo
-         */
-        private void uploadPicture(byte[] photo, String key) {
-            StorageReference path = storage.child(key + ".jpg");
-
-            UploadTask uploadTask = path.putBytes(photo);
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                    exception.printStackTrace();
-                }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    photoUri = taskSnapshot.getDownloadUrl();
-                }
-            });
-        }
-
-
-        private byte[] convertBitmapToByteArray(Bitmap bitmap) {
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            return stream.toByteArray();
-        }
-
-        private void takePicture(){
-            Intent camera = new Intent("android.media.action.IMAGE_CAPTURE");
-            File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
-            camera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
-            photoUri = Uri.fromFile(photo);
-            if (camera.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(camera, 1);
-            }
-        }
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode,resultCode,data);
-            if (requestCode == 1 && resultCode == RESULT_OK) {
-                Uri selectedImage = photoUri;
-                getContentResolver().notifyChange(selectedImage, null);
-                ImageView imageView = (ImageView) findViewById(R.id.ImageView);
-                ContentResolver cr = getContentResolver();
-                Bitmap bitmap;
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
-                    imageView.setImageBitmap(bitmap);
-                    photo = convertBitmapToByteArray(bitmap);
-                    Toast.makeText(this, selectedImage.toString(), Toast.LENGTH_LONG).show();
-                    time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-                    //addEventToDatabase(location, time, message, photo);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
-                            .show();
-                    Log.e("Camera", e.toString());
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = editText.getText().toString();
+                String location = getCurrentLocation();
+                if(location != null) {
+                    addEventToDatabase(getCurrentLocation(), message, photo);
+                    Toast.makeText(EventDetailsActivity.this, "Message sent", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EventDetailsActivity.this, "Message cannot be sent because your location is not found",
+                            Toast.LENGTH_SHORT).show();
                 }
 
             }
-        }
+        });
 
-        private String getCurrentLocation() {
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+    }
+
+
+    private String addEventToDatabase(String location, String message, byte[] photo) {
+        DatabaseReference events = database.child("events");
+        DatabaseReference newEvent = events.push();
+
+        String time = new SimpleDateFormat("yyyy.MM.dd_HH:mm:ss").format(new Date());
+        uploadPicture(photo, newEvent.getKey());
+        Event event = new Event(location, time, message, photoUri.toString());
+
+        newEvent.setValue(event);
+        return newEvent.getKey();
+    }
+
+    /**
+     * uploads picture and sets its uri in global variable photoUri.
+     * photoUri is not changed when uploading does not succeed.
+     * @param photo can be null
+     * @param key unique key for object we want to add to database, used to create unique path for photo
+     */
+    private void uploadPicture(byte[] photo, String key) {
+        StorageReference path = storage.child(key + ".jpg");
+
+        UploadTask uploadTask = path.putBytes(photo);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                exception.printStackTrace();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                photoUri = taskSnapshot.getDownloadUrl();
+            }
+        });
+    }
+
+
+    private byte[] convertBitmapToByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        return stream.toByteArray();
+    }
+
+    private void takePicture(){
+        Intent camera = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+        camera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+        photoUri = Uri.fromFile(photo);
+        if (camera.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(camera, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Uri selectedImage = photoUri;
+            getContentResolver().notifyChange(selectedImage, null);
+            ImageView imageView = (ImageView) findViewById(R.id.ImageView);
+            ContentResolver cr = getContentResolver();
+            Bitmap bitmap;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                imageView.setImageBitmap(bitmap);
+                photo = convertBitmapToByteArray(bitmap);
+                Toast.makeText(this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+
+                //addEventToDatabase(location, time, message, photo);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+                        .show();
+                Log.e("Camera", e.toString());
+            }
+
+        }
+    }
+
+
+    /**
+     * @return concatenation of latitude , longitude
+     * never throws an exception.
+     * in case of any exceptions returns null string.
+     */
+    private String getCurrentLocation() {
+        try {
             LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
             if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
@@ -173,8 +183,11 @@ public class EventDetailsActivity extends AppCompatActivity {
             }
             Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             return "" + location.getLatitude() + "," + location.getLongitude();
+        } catch(Exception e) {
+            return null;
         }
     }
+}
 
 
 
